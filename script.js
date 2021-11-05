615469502022573efb37d73a28239b4bfc536f9d
Jamie Flynt <jamflynt@gmail.com>
2021-11-04 21:44:17 -0400

    Basic functionality written for JS file.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const panels = document.querySelector(`.panel`)
+const panels = document.querySelectorAll(`.panel`)
+
+panels.forEach((panel) => {
+    console.log(panel);
+    panel.addEventListener(`click`, () => {
+        removeActiveClasses();
+        panel.classList.add(`active`);
+    });
+});
+
+function removeActiveClasses(){
+    panels.forEach((panel) => {
+        panel.classList.remove(`active`)
+    })
+}
